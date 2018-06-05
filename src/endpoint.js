a4c0322b493c39a523fb30e61d47e73f71180869
Vijay Khemlani <vkhemlan@gmail.com>
2018-06-05 17:54:30 -0400

    * Changed API endpoint * Added README

---
+++ src/endpoint.js
@@ -0,0 +1,6 @@
+export const endpoint = 'https://publicapi.solotodo.com/';
+// export const endpoint = 'https://api.solotodo.com/';
+
+// let endpoint = 'http://local.solotodo.com:8000/';
+// let endpoint = 'http://api.solotodo.com:8000/';
+// let endpoint = 'https://api.solotodo.com/';
